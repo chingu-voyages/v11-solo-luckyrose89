87c9e8a35ee2fdce23594d1271c03ce19fee0ea7
avidrutham <divyamathur89@gmail.com>
2019-09-12 18:58:36 +0530

    add cors and complete server middleware

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+// CORS & body parse middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+//Error handling middleware
+app.get("*", (req, res) => {
+  res.status(404);
+  res.send("Sorry the page you're looking for does not exist!");
+});
 
 app.listen(PORT, () => {
   console.log("App now listening on port: ", PORT);
